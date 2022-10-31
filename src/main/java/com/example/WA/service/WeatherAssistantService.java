731aca392740a7559476b2232a4da4f4e8646bb3
Zineddine Zahouani <43182889+zineddinezino@users.noreply.github.com>
2022-10-31 20:43:13 +0100

    Get Weather Prediction

--- src/main/java/com/example/WA/service/WeatherAssistantService.java
+++ src/main/java/com/example/WA/service/WeatherAssistantService.java
@@ -4,8 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 @AllArgsConstructor
 @Slf4j
 public class WeatherAssistantService {
+
+    public String getWeatherPrediction(String location, Date date){
+        var weather = "";
+        return switch (weather) {
+            case ("Rainy") -> "Take your umbrella";
+            case ("Sunny") -> "It will be worm today";
+            case ("Windy") -> "Don't forget your jacket";
+            case ("Stormy") -> "You better stay home";
+            default -> throw new IllegalStateException("Unexpected value: " + weather);
+        };
+    }
 }
